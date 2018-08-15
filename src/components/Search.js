2387dec0e3e48bc28b0b110f605f89f83afefbca
Carlos Alba <calba5141114@Carloss-MacBook-Pro.local>
2018-08-14 18:43:48 -0700

    bare version of the giphy app

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,41 +1,62 @@
 import React, { Component } from 'react';
-import config from './config'
-export default class Search extends Component{
+export default class Search extends Component {
 
-    constructor(){
+    constructor() {
+
         super();
         this.state = {
-            searchKeyword: '',
-            giphyImageData: [],
+            value: '',
+            giphyImageData: [<h2 key="0">There are currently no GIFS</h2>],
         };
 
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     // returns and processes JSON data from Giphy API
-    searchKeyword(keyWord){
+    searchKeyword(keyWord) {
         fetch(`http://api.giphy.com/v1/gifs/search?q=${keyWord}&api_key=D8qiB09X8jDja3OkYobyd1sI8qcFD8Tx`)
-        .then( data => { 
-            console.log(data)
-            return data.json })
-        .then(procData =>{
+            .then(data => {
+                // console.log(data)
+                // formats the json file so that it can be parsed out
+                return data.json()
+            })
+            .then(procData => {
 
-            console.log(procData.data);
+                // console.log(procData.data);
 
-            // let mappedData;
-            // this.setState({ giphyImageData: mappedData })
-
-
-        });
+                let mappedData;
+                mappedData = procData.data.map((x, count) => {
+                    return (
+                        <div className="gif" key={count}>
+                            <img src={x.images.original.url} alt="giphy" />
+                        </div>
+                    );
+                })
+                this.setState({ giphyImageData: mappedData });
+            });
     }
 
-    componentDidMount(){
-      this.searchKeyword('cat')
+    handleChange(event) {
+        this.setState({ value: event.target.value });
     }
-    
-    render(){
-        return(
+
+    handleSubmit(event) {
+        this.searchKeyword(this.state.value);
+        event.preventDefault();
+    }
+
+
+    render() {
+        return (
             <div>
-            <p>Working Search Function</p>
+                <form onSubmit={this.handleSubmit}>
+                    <input type="text" value={this.state.value} onChange={this.handleChange} placeholder="GIF" />
+                    <br/>
+                    <input type="submit" value="Submit" />
+                </form>
+                <br/>
+                {this.state.giphyImageData}
             </div>
         );
     }
