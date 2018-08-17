d01bbfa096653b68b9383ce1f530962feaff6291
Carlos Alba <calba5141114@Carloss-MacBook-Pro.local>
2018-08-16 19:52:47 -0700

    performance improvements

--- src/components/Search.js
+++ src/components/Search.js
@@ -10,34 +10,42 @@ export default class Search extends Component {
         super();
         this.state = {
             value: '',
-            giphyImageData: [<h2 key="0">There are currently no GIFS</h2>],
+            giphyImageData: [<h2 key="0">There are currently no GIFS.</h2>],
         };
 
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.purgeGiphy = this.purgeGiphy.bind(this);
+    }
+
+    // purges data and resets data in giphyImageData
+    purgeGiphy(){
+        this.setState({ giphyImageData: [<h2 key="0">There are currently no GIFS.</h2>] });
     }
 
     // returns and processes JSON data from Giphy API
     searchKeyword(keyWord) {
+
+        this.purgeGiphy();
+
         fetch(`http://api.giphy.com/v1/gifs/search?q=${keyWord}&api_key=${config.api_key}`)
             .then(data => {
-                // console.log(data)
-                // formats the json file so that it can be parsed out
                 return data.json()
             })
             .then(procData => {
-
-                // console.log(procData.data);
 
                 let mappedData;
                 mappedData = procData.data.map((x, count) => {
                     return (
                         <div className="gif" key={count}>
                             <img src={x.images.original.url} alt="giphy" />
+                            <p>{x.title}</p>
                         </div>
                     );
                 })
+                
                 this.setState({ giphyImageData: mappedData });
+
             });
     }
 
